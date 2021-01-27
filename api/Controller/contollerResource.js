8a39f8a1d8f0ecce45dd1542046cb57e779c3170
Emmanuel <emmanuelchinaza5777@gmail.com>
2021-01-27 15:23:01 +0100

    second commit

--- api/Controller/contollerResource.js
+++ api/Controller/contollerResource.js
@@ -1,10 +1,25 @@
 
 const getController = (req, res) =>{
-    res.send("Hello from getController")
+    const basicInfo = {
+    "name": "Chukwunazaekpere Obioma",
+    "github": "@Chukwunazaekpere",
+    "email": "emmanuelchinaza5777@gmail.com",
+    "mobile": "08169547035",
+    "twitter": "no twitter account"
+    }
+
+    const surname = basicInfo['name'].split(" ")[1];
+    res.status(200).json({
+        "message": `${surname}'s Basic Info`,
+        "status": "success",
+        "data": {
+            ...basicInfo
+        }
+    })
 }
 
 const validateRuleController = (req, res) =>{
-    res.send("Hello from validateRuleController")
+    console.log(req.body);
 
 }
 
--- api/Routes/routeResource.js
+++ api/Routes/routeResource.js
@@ -1,13 +1,16 @@
 import express from "express";
 const router = express.Router();
 
-
+// Assign required controller to respective route.
 import controller from "../Controller/contollerResource.js"
 
+// Destructure constituting controllers
 const { getController, validateRuleController } = controller;
 
+// Route for get
 router.get("/", getController)
 
+// Route for validation
 router.post("/validate-rule", validateRuleController)
 
 
